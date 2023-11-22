6df2865311b70f14bd3a95817195520fbf18728a
Nitradee8 <nbtt.nik@gmail.com>
2023-11-22 23:10:46 +0700

    edit css

--- src/components/AllProduct.jsx
+++ src/components/AllProduct.jsx
@@ -1,7 +1,36 @@
+import axios from "axios";
+import { useState } from "react"
+import { useEffect } from "react"
+
+
 export default function AllProduct() {
+
+    const [allProduct, setAllProduct] = useState([]);
+
+    useEffect(() => {
+        axios.get('/product/allproduct').then((res) => setAllProduct(res.data.allProduct))
+    }, [])
+
+    console.log(allProduct)
+
     return (
-        <div>
-            
+        <div className="grid grid-cols-2 gap-6">
+            {allProduct.map((el, index) => (
+                <div key={index} className="w-full">
+                    <div className="gap-10 rounded-2xl p-8 bg-stone-300 w-[800px] h-[300px]">
+                        <div className="flex gap-5">
+                            <img src={el.image} alt={el.title} width={100} height={50} />
+                            <div className="flex flex-col gap-5">
+                                <p className="text-xl">{el.bookname}</p>
+                                <p className="text-xl">{el.categoryId}</p>
+                                <p className="text-xl">{el.description}</p>
+                                <p className="text-xl" to="price" >{el.price}</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
--- src/page/AdminPage.jsx
+++ src/page/AdminPage.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react";
 import CreateProduct from "../components/CreateProduct";
+import AllProduct from "../components/AllProduct";
 
 
 export default function AdminPage() {
   const [mode, setMode] = useState();
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className=" bg-amber-100 p-5 w-[400px] h-full">
-        <div className="p-5 active:text-MonoColor-600 active:bg-Primary-light text-lg font-sans hover:font-serif">
-          <p onClick={() => setMode("CreateProduct")} >Create Product</p>
+    <>
+      <div className="flex justify-center items-center">
+        <div className=" bg-amber-100 p-5 w-[400px] h-screen">
+          <div className="p-5 active:text-MonoColor-600 active:bg-Primary-light text-lg font-sans hover:font-serif">
+            <p onClick={() => setMode("CreateProduct")} >Create Product</p>
+          </div>
+          <hr />
+          <div className="p-5 active:text-MonoColor-600 active:bg-Primary-light text-lg font-sans hover:font-serif">
+            <p onClick={() => setMode("AllProduct")} >All Product</p>
+          </div>
         </div>
-        <hr />
-        <div className="p-5 active:text-MonoColor-600 active:bg-Primary-light text-lg font-sans hover:font-serif">
-          <p onClick={() => setMode("DeleteProduct")} >All Product</p>
+        <div className="w-full">
+          <div className="flex w-full justify-center items-center">{mode === "CreateProduct" ? <CreateProduct /> : ""}</div>
+          <div className="flex w-full justify-center items-center">{mode === "AllProduct" ? <AllProduct /> : ""}</div>
         </div>
       </div>
-      <div className="w-full flex justify-center items-center">{mode === "CreateProduct" ? <CreateProduct /> : ""}
-      </div>
-    </div>
+
+
+    </>
+
   )
 }
